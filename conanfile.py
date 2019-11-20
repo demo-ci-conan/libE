6b9348d898e81e9e2dd4e245044a1af975b0e0b4
Niels van der Schans <Niels.vanderSchans@tomtom.com>
2019-11-20 17:05:17 +0100

    Set dependencies

--- conanfile.py
+++ conanfile.py
@@ -17,7 +17,11 @@ class LibC(ConanFile):
     exports_sources = "LICENSE" # to avoid build info bug
 
     def requirements(self):
-        self.requires("libB/0.0@demo/testing")
+        self.requires(
+            "libB/0.0@demo/testing",
+            "libG/0.0@demo/testing",
+            "libI/0.0@demo/testing",
+            )
 
     def build(self):
         cmake = CMake(self)
--- src/libE.cpp
+++ src/libE.cpp
@@ -2,9 +2,13 @@
 #include "libE/libE.h"
 
 #include <iostream>
-#include "libC/libC.h"
+#include "libB/libB.h"
+#include "libG/libG.h"
+#include "libI/libI.h"
 
 void hello_libC(int indent, const std::string& msg) {
     std::cout << std::string(indent, ' ') << "libE: " << msg << std::endl;
-    hello_libC(indent+1, "hello from libE");
+    hello_libB(indent+1, "hello from libE");
+	hello_libG(indent+1, "hello from libE");
+	hello_libI(indent+1, "hello from libE");
 }
