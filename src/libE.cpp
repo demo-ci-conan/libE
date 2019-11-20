df393bf76be257c1700c84251da950161450bf2e
Niels van der Schans <Niels.vanderSchans@tomtom.com>
2019-11-20 20:04:28 +0100

    Use header only lib

--- src/libE.cpp
+++ src/libE.cpp
@@ -5,12 +5,11 @@
 #include "libA/libA.h"
 #include "libB/libB.h"
 #include "libG/libG.h"
-#include "libI/libI.h"
+#include "libI/libI_headerOnly.h"
 
 void hello_libE(int indent, const std::string& msg) {
-    std::cout << std::string(indent, ' ') << "libE: " << msg << std::endl;
+    std::cout << std::string(indent, ' ') << "libE: " << msg << " - " << LIBI_VAR << std::endl;
     hello_libA(indent+1, "hello from libE");
     hello_libB(indent+1, "hello from libE");
 	hello_libG(indent+1, "hello from libE");
-	hello_libI(indent+1, "hello from libE");
 }
