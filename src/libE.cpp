090311db381ea61d91dd95fc7e5d21e80f2f852d
Niels van der Schans <Niels.vanderSchans@tomtom.com>
2019-11-20 18:09:35 +0100

    Add dependencies

--- src/libE.cpp
+++ src/libE.cpp
@@ -2,12 +2,14 @@
 #include "libE/libE.h"
 
 #include <iostream>
+#include "libA/libA.h"
 #include "libB/libB.h"
 #include "libG/libG.h"
 #include "libI/libI.h"
 
 void hello_libE(int indent, const std::string& msg) {
     std::cout << std::string(indent, ' ') << "libE: " << msg << std::endl;
+    hello_libA(indent+1, "hello from libE");
     hello_libB(indent+1, "hello from libE");
 	hello_libG(indent+1, "hello from libE");
 	hello_libI(indent+1, "hello from libE");
